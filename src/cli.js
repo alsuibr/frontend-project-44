6f45966c6a74db9adf3f6ffc76b9bd1cbcdd7388
Alsu Ibragimova <alsu.ibragimova@gmail.com>
2023-01-10 15:16:45 -0500

    step 3, commit 4

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const askNameAndGreet = () => {
-const playerName = readlineSync.question('May i have your name? ');
+const playerName = readlineSync.question('May I have your name? ');
 console.log('Hello, ' + playerName + '!');
 };
 
