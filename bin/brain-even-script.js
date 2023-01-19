bb88d45dc17e03e669374c5e5b6d00d4283f9970
Alsu Ibragimova <alsu.ibragimova@gmail.com>
2023-01-19 12:36:56 -0500

    step 5, commit 8, brain-even.js modified, brain-even-script.js added

---
+++ bin/brain-even-script.js
@@ -0,0 +1,42 @@
+import readlineSync from 'readline-sync';
+import { playerName } from "../src/cli.js";
+
+const gameEven = () => {
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const getRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+for (let i = 0; i < 3; i = i + 1) {
+
+    let randomNumber = getRandomNumber(1, 101);
+    console.log(`Question: ${randomNumber}`);
+
+    let usersAnswer;
+    const getAnswer = () => {
+        usersAnswer = readlineSync.question(`Your answer: `);
+    }
+    getAnswer();
+
+    let correctAnswer;
+    randomNumber % 2 === 0 ? correctAnswer = 'yes' : correctAnswer = 'no';
+
+    if (usersAnswer !== correctAnswer) {
+        console.log(`'${usersAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}!`);
+        break;
+    }
+
+    if (usersAnswer === correctAnswer) {
+        console.log('Correct!');
+    }
+
+    if (i === 2) {
+        console.log(`Congratulations, ${ playerName }!`);
+    }
+}
+};
+
+export { gameEven };
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,48 +1,9 @@
-import readlineSync from 'readline-sync';
-import { playerName } from "../src/cli.js";
+#!/usr/bin/env node
+
 import gameIntro from "../src/cli.js";
+import { gameEven } from "./brain-even-script.js";
 
-const gameEven = () => {
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-const getRandomNumber = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-for (let i = 0; i < 3; i = i + 1) {
-
-    let randomNumber = getRandomNumber(1, 101);
-    console.log(`Question: ${randomNumber}`);
-
-    let usersAnswer;
-    const getAnswer = () => {
-        usersAnswer = readlineSync.question(`Your answer: `);
-    }
-    getAnswer();
-
-    let correctAnswer;
-    randomNumber % 2 === 0 ? correctAnswer = 'yes' : correctAnswer = 'no';
-
-    if (usersAnswer !== correctAnswer) {
-        console.log(`'${usersAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}!`);
-        break;
-        i === 3;
-    }
-
-    if (usersAnswer === correctAnswer) {
-        console.log('Correct!');
-    }
-
-    if (i === 2) {
-        console.log(`Congratulations, ${ playerName }!`);
-    }
-}
-};
 
 gameIntro();
 gameEven();
-
-export { gameEven };
 
